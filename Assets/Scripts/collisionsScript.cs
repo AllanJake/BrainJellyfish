e5f4744a0d98b57b0c2ca68308133e083ec6bc50
Jake Allan <1501716@uad.ac.uk>
2017-10-07 17:44:21 +0100

    Collisions
    Collisions prototype code

---
+++ Assets/Scripts/collisionsScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionsScript : MonoBehaviour {
+
+    public bool red = false;
+    public bool blue = false;
+    public bool green = false;
+    public bool mystery = false;
+
+    //detect collisions with triggers
+    void OnTriggerEnter(Collider other)
+    {
+        //movement boundaries
+        if (other.CompareTag("redBox"))
+            red = true;
+
+        if (other.CompareTag("blueBox"))
+            blue = true;
+
+        if (other.CompareTag("greenBox"))
+            green = true;
+
+        if (other.CompareTag("mysteryBox"))
+            mystery = true;
+    }
+}
