7a872fb4a307b97d7e91de0b7dfefcc8058c9e71
jakeallan-abertay <1501716@uad.ac.uk>
2018-02-26 18:15:47 +0000

    Main Menu

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject controls;
+
+    bool isControls = false;
+
+    private void startButton()
+    {
+        // Start the game
+        SceneManager.LoadScene("Prototype");
+    }
+
+    private void controlsButton()
+    {
+        isControls = !isControls;
+        if (isControls)
+        {
+            // Show Controls
+            // First hide the buttons and text
+            menu.SetActive(false);
+            // Show the controls screen text
+            controls.SetActive(true);
+        }
+        else
+        {
+            menu.SetActive(true);
+            controls.SetActive(false);
+        }
+    }
+}
